b368d5a89d7fd3447851ebdb7ea126e78487a1d1
lamprose <29279979+lamprose@users.noreply.github.com>
2026-04-03 18:58:16 +0800

    fix: `auto-preview-dev-auto.yml` bug

--- .github/tel-bot.py
+++ .github/tel-bot.py
@@ -6,26 +6,33 @@ import os
 # --- Environment Variables ---
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
+APK_PATH = os.environ.get("APK_PATH")
+COMMIT_MESSAGE = os.environ.get("COMMIT_MESSAGE")
 
 
-async def send_telegram_files(files, message):
+async def send_telegram_files():
+    if not BOT_TOKEN or not CHAT_ID:
+        print("[-] BOT_TOKEN and CHAT_ID environment variables must be set.")
+        return
+    if not APK_PATH:
+        print("[-] No files to send.")
+        return
     """
     Connects to Telegram and sends the specified files as a group message.
     """
     url = "https://api.telegram.org/bot{0}/sendDocument".format(BOT_TOKEN)
     # Send the files together as an album/group
-    files = map(
-        lambda f: (
+    files = [
+        (
             "document",
             (
-                f.split("/")[-1],  # Use the file name from the path
-                open(f, "rb"),
+                APK_PATH.split("/")[-1],
+                open(APK_PATH, "rb"),
                 "application/octet-stream",
             ),
-        ),
-        files,
-    )
-    payload = {"chat_id": CHAT_ID, "caption": message}
+        )
+    ]
+    payload = {"chat_id": CHAT_ID, "caption": COMMIT_MESSAGE}
 
     requests.request("POST", url, data=payload, files=files)
     print("[+] Files sent successfully.")
@@ -36,10 +43,10 @@ if __name__ == "__main__":
         # Get all file paths from command-line arguments
         message = sys.argv[1]
         apk_files = sys.argv[2:]
-        print(f"[+] Found files to upload: {apk_files}")
+        print(f"[+] Found files to upload: {APK_PATH}")
         try:
             # Run the asynchronous function
-            asyncio.run(send_telegram_files(apk_files, message))
+            asyncio.run(send_telegram_files())
         except Exception as e:
             print(f"[-] An error occurred: {e}")
     else:
